be4c1ec5a00d07d3bf59aa59f542bc07aa2dc545
prooF <proof87@gmail.com>
2023-04-07 20:29:33 +0200

    updated node_server and base template

--- node_server/index.js
+++ node_server/index.js
@@ -4,8 +4,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.set('views', './templates')
+
 app.get("/", (req, res) => {
     res.json({ message: "Hello world!" });
+});
+app.get("/base", (req, res) => {
+    res.render("base")
 });
 
 app.listen(PORT, () => {
