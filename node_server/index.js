1781c0d3e3da4ab4157365c354cf9b866adf7b5f
prooF <proof87@gmail.com>
2023-04-14 02:09:48 +0200

    Express fix in index.js, base.html extension, demo.js for React testing

--- node_server/index.js
+++ node_server/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
+var path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.engine('.html', require('ejs').__express);
+
 app.set('views', './templates')
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello world!" });
 });
 app.get("/base", (req, res) => {
-    res.render("base")
+    res.render("base.html")
 });
 
 app.listen(PORT, () => {
---
+++ public/demo.js
@@ -0,0 +1,25 @@
+import Button from '../react-bootstrap/Button'
+
+var main_feed = document.querySelector("#main-feed");
+main_feed.innerText = 'main feed placeholder'
+var feed_data = [{
+    id: 0,
+    author: 1,
+    title: 'HalihóÓ',
+    content: 'Ez az első poszt',
+  },
+  {
+    id: 1,
+    author: 1,
+    title: null,
+    content: 'Lorem ipsum',
+  },
+  {
+    id: 1,
+    author: 1,
+    title: "Lorem",
+    content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+]
+var authors = [{id: 1, name: "Bálint"}]
+
