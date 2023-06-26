d0c208d3d4f65f8e81c1dd590af86a38a386be2f
prooF <proof87@gmail.com>
2023-06-26 02:51:44 +0200

    Mainfeed setState fix, debug opts

--- src/mainfeed.js
+++ src/mainfeed.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { NewsFeedPostForm } from './postforms';
+import Form from 'react-bootstrap/Form';
 
 
 export function BasicPost(props) {
@@ -23,28 +23,43 @@ export function NewsFeed({ postdata }) {
   let [posts, setPosts] = useState(postdata)
 
   function handleSubmit(e) {
-    // Prevent the browser from reloading the page
-    e.preventDefault();
+      // Prevent the browser from reloading the page
+      e.preventDefault();
 
-    // Read the form data
-    const form = e.target;
-    const formData = new FormData(form);
+      // Read the form data
+      const form = e.target;
+      const formData = new FormData(form);
 
-    // Passing formData to the API:
-    //fetch('/some-api', { method: form.method, body: formData });
+      // Passing formData to the API:
+      fetch('/newsfeed/sendpost', { method: form.method, body: formData });
 
-    // Or you can work with it as a plain object:
-    const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson);
-    setPosts([formJson])
-}
+      // Or you can work with it as a plain object:
+      const formJson = Object.fromEntries(formData.entries());
+      console.log(formJson);
+      setPosts([formJson, ...posts])
+  }
+
+  function NewsFeedPostForm() {
+    return (
+      <Form id='newsfeedpostform' className="my-2" onSubmit={handleSubmit} method='POST'>
+        <Form.Label visuallyHidden="true">Posztod szövege</Form.Label>
+        <Form.Control name="text" id="posttext" as="textarea" placeholder="Posztod szövege">
+        </Form.Control>
+        <input type="hidden" name="author" id="post-author"/>
+
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
+    );
+  }
 
   posts.sort((a, b) => b.key-a.key)
-  let renderedContent = [ <NewsFeedPostForm onSubmit={handleSubmit} /> ]
+
+  let renderedContent = [ <NewsFeedPostForm /> ]
   renderedContent.push(
     posts.map((postProps) => {
       return <BasicPost key={postProps.key} {...postProps} />
     })    
   )
+
   return renderedContent
 }
