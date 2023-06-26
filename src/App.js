0f9d250d0ab91c12c66e54333d532ceb15415132
prooF <proof87@gmail.com>
2023-06-26 02:52:32 +0200

    IndexedDB initial

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { NewsFeed } from './mainfeed';
 import './scss/styles.scss';
 
 
+let errorlog = []
+const statusInfoBox = document.querySelector("#debug-info")
 //const postformroot = createRoot(document.getElementById('post-form'))
 //postformroot.render(<NewsFeedPostForm />)
 
@@ -10,4 +12,23 @@ const root = createRoot(document.getElementById('main-feed'))
 const demodata = require('../tests/newsfeed_test_data')
 const demoposts = demodata.posts || []
 root.render(<NewsFeed postdata={ demoposts } />);
- 
+
+let idb
+const request = window.indexedDB.open("NewsFeedDatabase", 1);
+
+request.onerror = (event) => {
+    console.error(`Error creating IndexedDB: ${event}`)
+    errorlog.push(event)
+    errorlog.length > 100 && errorlog.shift() 
+};
+
+request.onsuccess = (event) => {
+    idb = event.target.result
+    console.log("IDB Success!")
+    statusInfoBox ? statusInfoBox.innerHTML = "DB success" :
+
+    idb.onerror = (event) => {
+        // Generic error handler for all errors targeted at IndexedDB requests
+        console.error(`IndexedDB error: ${event.target.errorCode}`);
+    };
+};
