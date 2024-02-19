e438099b53857654aaeacc79ad351aa3811dbf56
prooF <proof87@gmail.com>
2024-02-19 18:15:39 +0100

    register fix + view updates

--- brahma/brahma/urls.py
+++ brahma/brahma/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     re_path('app/*', TemplateView.as_view(template_name = "brahma/app.html"), name="appview"),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view())
+    path('', IndexView.as_view(), name="home")
 ]
 
 urlpatterns += [
