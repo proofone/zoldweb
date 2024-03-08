508279a85237d792cee80e0f102fe71d9ec3e2a5
prooF <proof87@gmail.com>
2024-03-08 18:45:04 +0100

    Entities minor changes

--- brahma/entities/tests.py
+++ brahma/entities/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from entities.models import User, Community, OtherEntity, Location
 
-# Create your tests here.
+
+class UserTest(TestCase):
+    def test_create_user(self):
+        u = User.objects.create(username="test1", password="test1", email="test1@example.com")
+        self.assertIsInstance(u, User)
+        self.assertTrue(hasattr(u, 'pk'))
+
+    def test_register_correct(self):
+        c = Client()
+        response = c.post("/register/", {
+            "username": "test2", 
+            "password": "test2", 
+            "email": "test2@example.com"
+            })
+        
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_no_email(self):
+        c = Client()
+        response = c.post("/register/", {
+            # "username": "test2", 
+            "password": "test2", 
+            "email": "test2@example.com"
+            })
+        
+        self.assertEqual(response.status_code, 403)
+        
